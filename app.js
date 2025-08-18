3ebf9b453f6be80d88302a196e282e4020242349
Elivelton Rocha <e_liveltonsouza@hotmail.com>
2025-08-18 17:58:00 -0400

    função adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 let listaAmigos = []
+
+// Função para adicionar um novo amigo.
+function adicionarAmigo() {
+    // acessa o conteúdo digitado pela usuário.
+    let nome = document.getElementById('amigo').value;
+    
+ 
+}
