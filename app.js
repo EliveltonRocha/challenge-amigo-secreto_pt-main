cc75d0eba6f178154f743c9cfa34dfac32435647
Elivelton Rocha <e_liveltonsouza@hotmail.com>
2025-08-18 18:02:45 -0400

    limpa o campo de digitação

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ function adicionarAmigo() {
     // acessa o conteúdo digitado pela usuário.
     let nome = document.getElementById('amigo').value;
     
- 
+ // Adiciona o nome do amigo para o final da lista.
+    listaAmigos.push(nome);
+
+    // Atualiza o elemento com id 'listaAmigos' para mostrar os nomes e separa com um hifem
+    document.getElementById('listaAmigos').textContent = listaAmigos.join(' - ');
+    
+    // Limpa o campo de nomes após clicar em adicionar.
+    document.getElementById('amigo').value = '';
 }
