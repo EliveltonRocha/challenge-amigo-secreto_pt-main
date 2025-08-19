be2f40563a1bee48c5025c0d3d60dac2dd36d882
Elivelton Rocha <e_liveltonsouza@hotmail.com>
2025-08-19 17:25:20 -0400

    verifica se o nome já foi digitado

--- app.js
+++ app.js
@@ -9,11 +9,26 @@ function adicionarAmigo() {
         alert('Por favor, digite um nome válido.');
         return;
     }
+    // verifica nomes duplicados maiusculos e minúsculos.
+    let nomeJaExiste = false;
+    for (let amigo of listaAmigos) {
+        // converte os nomes para minúsculas 
+        if (amigo.toLowerCase() === nome.toLowerCase()) {
+            nomeJaExiste = true;
+            break;
+        }
+    }
+    if (nomeJaExiste) {
+        // SE o nome já existir, exibe um alerta.
+        alert('Este nome já foi adicionado!');
+        return;
+    }
      // Adiciona o nome do amigo para o final da lista.
     listaAmigos.push(nome);
     // Limpa o campo de nomes após clicar em adicionar.
     document.getElementById('amigo').value = '';
-     atualizarLista();
+
+    atualizarLista();
 }
 
 function atualizarLista() {
